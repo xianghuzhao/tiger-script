9b3671bea73f788cf626767b554c69f50a7ae733
Xianghu Zhao <xianghuzhao@gmail.com>
2019-04-12 16:32:45 +0800

    Auto ssh scripts

---
+++ auto-login/tunnelin.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 PARAMFILE" >&2
+  exit 1
+fi
+
+paramfile=$1
+echo "Open $paramfile"
+
+ls $paramfile
+. "$paramfile"
+
+host=$(shuf -n1 -e $hosts)
+
+cd $(dirname "$0")
+
+./autotunnel.expect "$user@$host" "$password"
