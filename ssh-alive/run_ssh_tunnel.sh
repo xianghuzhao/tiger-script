da3df33fc5ada1c5707c8ac5d53836586bfb31e0
Xianghu Zhao <xianghuzhao@gmail.com>
2019-09-06 11:48:28 +0800

    Setup config for ssh-alive

--- ssh-alive/run_ssh_tunnel.sh
+++ ssh-alive/run_ssh_tunnel.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/bin/ssh -o 'ExitOnForwardFailure yes' -N -T -C -R 22222:localhost:22 tunnel
+. ./config
+
+/usr/bin/ssh -o 'ExitOnForwardFailure yes' -N -T -C -R ${remote_port}:localhost:${local_port} ${remote_host}
